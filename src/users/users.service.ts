5269ee28cabc40f4953d5d3ff8ade6b9178cf362
Skrestall <sergiyshmatko078@gmail.com>
2024-03-23 18:06:59 +0200

    create method for creating user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,52 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { HttpException, Injectable } from '@nestjs/common'
+import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
+import { Repository } from 'typeorm'
+import { User } from './entities/user.entity'
+import * as bcrypt from 'bcryptjs'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Message } from '@common/types/message.types'
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+    constructor(
+        @InjectRepository(User)
+        private readonly usersRepository: Repository<User>,
+    ) {}
 
-  findAll() {
-    return `This action returns all users`;
-  }
+    /**
+     * Create new user
+     * @param {CreateUserDto} createUserDto
+     * @returns {Promise<Message>}
+     */
+    public async create(createUserDto: CreateUserDto): Promise<Message> {
+        const user = await this.usersRepository.findOne({
+            where: { phone: createUserDto.phone },
+        })
+        if (user) throw new HttpException('User already exists', 400)
+        const hashPassword = await bcrypt.hash(createUserDto.password, 10)
+        const newUser = this.usersRepository.create({
+            ...createUserDto,
+            password: hashPassword,
+            createdAt: new Date(),
+        })
+        await this.usersRepository.save(newUser)
+        return { message: 'User created successfully' }
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+    public findAll() {
+        return `This action returns all users`
+    }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+    public findOne(id: number) {
+        return `This action returns a #${id} user`
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+    public update(id: number, updateUserDto: UpdateUserDto) {
+        return `This action updates a #${id} user`
+    }
+
+    public remove(id: number) {
+        return `This action removes a #${id} user`
+    }
 }
