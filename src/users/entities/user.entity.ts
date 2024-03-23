6f2f0c8cb07d5bc5d86e3290345efd8cb3bcc6ba
Skrestall <sergiyshmatko078@gmail.com>
2024-03-23 17:39:04 +0200

    create user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity('users')
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column({ unique: true })
+    phone: string
+
+    @Column()
+    name: string
+
+    @Column()
+    password: string
+
+    @Column({ name: 'created_at' })
+    createdAt: Date
+}
