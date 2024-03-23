2a0950329b901808046c9a220be03f1e6cc880b0
Skrestall <sergiyshmatko078@gmail.com>
2024-03-23 17:49:11 +0200

    swagger for entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,19 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 
 @Entity('users')
 export class User {
+    @ApiProperty({ type: String, example: '1', description: 'user id' })
     @PrimaryGeneratedColumn('uuid')
     id: string
 
+    @ApiProperty({
+        type: String,
+        example: '+1234567890',
+        description: 'phone number',
+    })
     @Column({ unique: true })
     phone: string
 
+    @ApiProperty({ type: String, example: 'John', description: 'user name' })
     @Column()
     name: string
 
+    @ApiProperty({
+        type: String,
+        example: 'qwerty1234',
+        description: 'user password',
+    })
     @Column()
     password: string
 
+    @ApiProperty({ type: Date, example: '2022-01-01T00:00:00.000Z' })
     @Column({ name: 'created_at' })
     createdAt: Date
 }
