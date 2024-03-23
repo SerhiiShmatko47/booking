ba29ecb4c603eea32714fa8e4981f8d34d5b8844
Skrestall <sergiyshmatko078@gmail.com>
2024-03-23 17:11:59 +0200

    configure migrations

---
+++ src/migrations/1711206565346-lock.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class Lock1711206565346 implements MigrationInterface {
+    public async up(_queryRunner: QueryRunner): Promise<void> {}
+
+    public async down(_queryRunner: QueryRunner): Promise<void> {}
+}
