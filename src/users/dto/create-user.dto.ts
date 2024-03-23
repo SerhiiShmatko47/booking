415018b1294541cc3dfadbb68918391dc5a8b9d3
Skrestall <sergiyshmatko078@gmail.com>
2024-03-23 17:47:21 +0200

    swagger for dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger'
+
 export class CreateUserDto {
+    @ApiProperty({
+        type: String,
+        example: '+1234567890',
+        description: 'Phone number',
+        required: true,
+    })
     public readonly phone: string
+
+    @ApiProperty({
+        type: String,
+        example: 'John',
+        description: 'User name',
+        required: true,
+    })
     public readonly name: string
+
+    @ApiProperty({
+        type: String,
+        example: 'qwerty1234',
+        description: 'User password',
+        required: true,
+    })
     public readonly password: string
 }
