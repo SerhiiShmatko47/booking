68ed6835345217f4469fb6498f76f7b1d9b9b1f2
Skrestall <sergiyshmatko078@gmail.com>
2024-03-26 14:03:23 +0200

    update users module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,111 +1,41 @@
 import {
     Controller,
     Get,
-    Post,
     Body,
     Patch,
-    Param,
     Delete,
     HttpStatus,
     HttpCode,
-    Query,
     UsePipes,
     UseInterceptors,
     ClassSerializerInterceptor,
     UseGuards,
 } from '@nestjs/common'
 import { UsersService } from './users.service'
-import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
 import {
     ApiBadRequestResponse,
     ApiBearerAuth,
     ApiBody,
-    ApiCreatedResponse,
     ApiOkResponse,
     ApiOperation,
-    ApiParam,
-    ApiQuery,
     ApiTags,
 } from '@nestjs/swagger'
 import { User } from './entities/user.entity'
 import { Message } from '@common/types/message.types'
-import { GetUsersPipe } from '@common/pipe/get-users'
 import { UserValidationPipe } from '@common/pipe/user-validation'
-import { RoleGuard } from '@common/guards/role/role.guard'
-import { Role } from '@common/enums/role.enum'
-import { Roles } from '@common/decorators/role.decorator'
 import { AuthGuard } from '@common/guards/auth/auth.guard'
+import { UserContextInterceptor } from '@common/interceptors/user-context'
+import { UserContext } from '@common/decorators/user-context.decorator'
 
 @ApiTags('users')
 @Controller('users')
-@Roles(Role.ADMIN)
-@UseGuards(AuthGuard, RoleGuard)
+@UseGuards(AuthGuard)
+@UseInterceptors(UserContextInterceptor)
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
-    @ApiOperation({ summary: 'Create user' })
-    @ApiBody({
-        type: CreateUserDto,
-    })
-    @ApiCreatedResponse({
-        status: HttpStatus.CREATED,
-        type: User,
-    })
-    @ApiBadRequestResponse({
-        status: HttpStatus.BAD_REQUEST,
-        schema: {
-            example: {
-                message: 'User already exists',
-            },
-        },
-    })
-    @ApiBearerAuth()
-    @Post()
-    @UsePipes(new UserValidationPipe())
-    @HttpCode(HttpStatus.CREATED)
-    @UseInterceptors(ClassSerializerInterceptor)
-    public async create(@Body() createUserDto: CreateUserDto): Promise<User> {
-        return this.usersService.create(createUserDto)
-    }
-
-    @ApiOperation({ summary: 'Get all user' })
-    @ApiQuery({
-        type: Number,
-        name: 'take',
-    })
-    @ApiQuery({
-        type: Number,
-        name: 'skip',
-    })
-    @ApiOkResponse({
-        status: HttpStatus.OK,
-        type: [User],
-    })
-    @ApiBadRequestResponse({
-        status: HttpStatus.BAD_REQUEST,
-        schema: {
-            example: {
-                message: 'take and skip must be numbers',
-            },
-        },
-    })
-    @ApiBearerAuth()
-    @Get()
-    @UsePipes(new GetUsersPipe())
-    @UseInterceptors(ClassSerializerInterceptor)
-    public async findAll(
-        @Query('take') take: number,
-        @Query('skip') skip: number,
-    ): Promise<User[]> {
-        return this.usersService.findAll(take, skip)
-    }
-
-    @ApiOperation({ summary: 'Get user by id' })
-    @ApiParam({
-        name: 'userId',
-        type: String,
-    })
+    @ApiOperation({ summary: 'Get yourself' })
     @ApiOkResponse({
         status: HttpStatus.OK,
         type: User,
@@ -119,17 +49,13 @@ export class UsersController {
         },
     })
     @ApiBearerAuth()
-    @Get(':userId')
+    @Get()
     @UseInterceptors(ClassSerializerInterceptor)
-    public async findOne(@Param('userId') userId: string): Promise<User> {
+    public async findOne(@UserContext('id') userId: string): Promise<User> {
         return this.usersService.findOne(userId)
     }
 
-    @ApiOperation({ summary: 'Update user' })
-    @ApiParam({
-        type: String,
-        name: 'userId',
-    })
+    @ApiOperation({ summary: 'Update yourself' })
     @ApiBody({
         type: UpdateUserDto,
     })
@@ -150,21 +76,17 @@ export class UsersController {
         },
     })
     @ApiBearerAuth()
-    @Patch(':userId')
+    @Patch()
     @UsePipes(new UserValidationPipe())
     @HttpCode(HttpStatus.CREATED)
     public async update(
-        @Param('userId') userId: string,
+        @UserContext('id') userId: string,
         @Body() updateUserDto: UpdateUserDto,
     ): Promise<Message> {
         return this.usersService.update(userId, updateUserDto)
     }
 
-    @ApiOperation({ summary: 'Delete user' })
-    @ApiParam({
-        type: String,
-        name: 'userId',
-    })
+    @ApiOperation({ summary: 'Delete yourself' })
     @ApiOkResponse({
         status: HttpStatus.OK,
         schema: {
@@ -182,8 +104,8 @@ export class UsersController {
         },
     })
     @ApiBearerAuth()
-    @Delete(':userId')
-    public async remove(@Param('userId') userId: string): Promise<Message> {
+    @Delete()
+    public async remove(@UserContext('id') userId: string): Promise<Message> {
         return this.usersService.remove(userId)
     }
 }
